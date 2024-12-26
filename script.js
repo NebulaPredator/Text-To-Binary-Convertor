cd2e4c26c1f19bac4441da08cf16c030d1466933
GarudaTheGuardian <mananth545@gmail.com>
2024-12-26 18:01:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,54 +18,11 @@ function translate() {
             .map(char => char.charCodeAt(0).toString(2).padStart(8, '0'))
             .join(" ");
     }
-// Convert decimal to binary and hexadecimal
-function convertFromDecimal() {
-  const decimalInput = document.getElementById('decimal').value;
-  const binaryInput = document.getElementById('binary');
-  const hexadecimalInput = document.getElementById('hexadecimal');
-  if (decimalInput !== '') {
-    const decimalValue = parseInt(decimalInput, 10);
-    binaryInput.value = decimalValue.toString(2);
-    hexadecimalInput.value = decimalValue.toString(16).toUpperCase();
-  } else {
-    binaryInput.value = '';
-    hexadecimalInput.value = '';
-  }
-}
-// Convert binary to decimal and hexadecimal
-function convertFromBinary() {
-  const binaryInput = document.getElementById('binary').value;
-  const decimalInput = document.getElementById('decimal');
-  const hexadecimalInput = document.getElementById('hexadecimal');
-  if (/^[01]+$/.test(binaryInput)) {
-    const decimalValue = parseInt(binaryInput, 2);
-    decimalInput.value = decimalValue;
-    hexadecimalInput.value = decimalValue.toString(16).toUpperCase();
-  } else {
-    decimalInput.value = '';
-    hexadecimalInput.value = '';
-  }
-}
-// Convert hexadecimal to decimal and binary
-function convertFromHexadecimal() {
-  const hexadecimalInput = document.getElementById('hexadecimal').value;
-  const decimalInput = document.getElementById('decimal');
-  const binaryInput = document.getElementById('binary');
 
     // Display the result in the <p id="result">
     document.getElementById("result").innerText = output;
-  if (/^[0-9A-Fa-f]+$/.test(hexadecimalInput)) {
-    const decimalValue = parseInt(hexadecimalInput, 16);
-    decimalInput.value = decimalValue;
-    binaryInput.value = decimalValue.toString(2);
-  } else {
-    decimalInput.value = '';
-    binaryInput.value = '';
-  }
 }
 
-// Add an event listener to the button to call translate when clicked
-document.getElementById("translateButton").addEventListener("click", translate);
 // Convert text to binary
 function convertFromText() {
   const textInput = document.getElementById('text').value;
@@ -80,6 +37,7 @@ function convertFromText() {
     binaryTextInput.value = '';
   }
 }
+
 // Convert binary text to plain text
 function convertFromBinaryText() {
   const binaryTextInput = document.getElementById('binaryText').value;
@@ -95,3 +53,6 @@ function convertFromBinaryText() {
     textInput.value = '';
   }
 }
+
+// Add an event listener to the button to call translate when clicked
+document.getElementById("translateButton").addEventListener("click", translate);
